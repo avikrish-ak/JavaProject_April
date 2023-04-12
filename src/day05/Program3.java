3536d3998f632ec0391925e608ac38d3b0a1592d
Avinash Krishnamoorthy <saiavinashkrishnamoorthy@gmail.com>
2023-04-12 20:32:47 +0530

    day05
    Overloading

---
+++ src/day05/Program3.java
@@ -0,0 +1,14 @@
+package day05;
+
+public class Program3 {
+
+	public static void main(String[] args) {
+		Program3 x= new Program3();
+		x.main(2,3);
+	}
+	
+	public void main(int a,int b) {
+		System.out.println((a*a)+(b*b)+(2*a*b));
+	}
+
+}
