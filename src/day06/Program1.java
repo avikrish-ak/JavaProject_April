121a779ede25e32f69fc3f723831c3f10ba95341
Avinash Krishnamoorthy <saiavinashkrishnamoorthy@gmail.com>
2023-04-21 20:06:31 +0530

    day08

---
+++ src/day06/Program1.java
@@ -0,0 +1,13 @@
+package day06;
+
+public class Program1 {
+
+	protected void cal(int a,int b) {
+		System.out.println("Program1 - Calculator");
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+	}
+
+}
---
+++ src/day06/TataPunch.java
@@ -0,0 +1,42 @@
+package day06;
+
+public class TataPunch extends TataCarSpec{
+
+	public static void main(String[] args) {
+		TataCarSpec tatapunch = new TataPunch();
+		tatapunch.displayHeight();//child class override method
+		tatapunch.displayModelNumber();//parent class method its static
+
+	}
+	
+	public static void displayModelNumber()
+	{
+		System.out.println("TatPunch Modelnumber is 123456");
+	}
+	
+	
+	@Override
+	public void displayHeight()
+	{
+		System.out.println("Tata Punch Height is 4.5f");
+	}
+	
+	@Override
+	public void displayWidth()
+	{
+		System.out.println("Tata Punch width is 6.5f");
+	}
+	
+	@Override
+	public void displaySpeed()
+	{
+		System.out.println("Tata Punch Speed is 110KM/Hr");
+	}
+	
+	@Override
+	public void displayPrice()
+	{
+		System.out.println("Tata Punch Price is 9.5L");
+	}
+
+}
